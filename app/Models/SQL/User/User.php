0957e12a3f5887909be9a828fa4c37f5e606b33f
schmev <86550672+schmev91@users.noreply.github.com>
2024-09-23 16:22:40 +0700

    modify User model

--- app/Models/SQL/User/User.php
+++ app/Models/SQL/User/User.php
@@ -3,9 +3,45 @@
 namespace App\Models\SQL\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'fullname',
+        'email',
+        'phone',
+        'password',
+     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        // 'remember_token',
+     ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            // 'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+         ];
+    }
 }
