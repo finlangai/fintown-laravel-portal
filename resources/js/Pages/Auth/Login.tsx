7483713d6fe9e125e446604dbbba266b04d48fbd
schmev <86550672+schmev91@users.noreply.github.com>
2024-09-27 10:23:56 +0700

    fix: hot fix component import error

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -21,7 +21,7 @@ export default function Login({
   });
   const submit: FormEventHandler = (e) => {
     e.preventDefault();
-    console.log('submitting' , data)
+    console.log("submitting", data);
     post(route("login"), {
       onFinish: () => reset("password"),
     });
