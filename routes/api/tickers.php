291c1ae5873370c0cfeecce62a7d4e581a5aea99
schmev <86550672+schmev91@users.noreply.github.com>
2024-10-16 16:43:57 +0700

    feature: best ticker endpoint for dashboard homepage

--- routes/api/tickers.php
+++ routes/api/tickers.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\API\Tickers\BestTickerController;
 use App\Http\Controllers\API\Tickers\IndustryTickersController;
 use App\Http\Controllers\API\Tickers\TickersController;
 use App\Http\Controllers\API\Tickers\TopGainerTickersController;
@@ -10,6 +12,8 @@ Route::get("tickers", TickersController::class);
 Route::prefix("tickers")->group(function () {
     Route::get("total", [TickersController::class, "total"]);
     Route::get("overview", [TickersController::class, "overview"]);
+    Route::get("best", BestTickerController::class);
+
     Route::get("top-gainers", TopGainerTickersController::class);
     Route::get("top-revenue", TopRevenueTickersController::class);
     Route::get("industry", IndustryTickersController::class);
