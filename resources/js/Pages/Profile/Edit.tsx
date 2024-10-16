c2da10f1a6a8b081741077b42605c2166debe612
schmev <86550672+schmev91@users.noreply.github.com>
2024-10-16 11:22:41 +0700

    fix type error on tsx files

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -5,55 +5,55 @@ import UpdateProfileInformationForm from "@/Pages/Profile/Partials/UpdateProfile
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/Types";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb"
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/Components/UI/breadcrumb";
 export default function Edit({
   mustVerifyEmail,
   status,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
   return (
-    <AuthenticatedLayout header={false} >
-        <Head title="Profile" />
-        <div className="py-12">
-            <div className="space-y-6 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-            <Breadcrumb>
-                <BreadcrumbList >
-                    <BreadcrumbItem>
-                    <BreadcrumbLink href="/" >Welcome</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator  />
-                    <BreadcrumbItem>
-                    <BreadcrumbLink href="dashboard" >Dashboard</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator  />
-                    <BreadcrumbItem>
-                    <BreadcrumbPage className="text-white">Profile</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
+    <AuthenticatedLayout header={false}>
+      <Head title="Profile" />
+      <div className="py-12">
+        <div className="space-y-6 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/">Welcome</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="dashboard">Dashboard</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage className="text-white">Profile</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
 
-                <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
-                    <UpdateProfileInformationForm
-                        mustVerifyEmail={mustVerifyEmail}
-                        status={status}
-                        className="max-w-xl"
-                    />
-                </div>
+          <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
+            <UpdateProfileInformationForm
+              mustVerifyEmail={mustVerifyEmail}
+              status={status}
+              className="max-w-xl"
+            />
+          </div>
 
-                <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
-                    <UpdatePasswordForm className="max-w-xl" />
-                </div>
+          <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
+            <UpdatePasswordForm className="max-w-xl" />
+          </div>
 
-                <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
-                    <DeleteUserForm className="max-w-xl" />
-                </div>
-            </div>
+          <div className="bg-white shadow p-4 sm:p-8 sm:rounded-lg">
+            <DeleteUserForm className="max-w-xl" />
+          </div>
         </div>
+      </div>
     </AuthenticatedLayout>
   );
 }
