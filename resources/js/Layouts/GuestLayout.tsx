4a47949f03ebab156a658370938ee0baf2efde39
icebingsu <botchat549@gmail.com>
2024-10-20 04:33:36 +0700

    update header drowdown

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,6 +1,7 @@
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import { Link } from "@inertiajs/react";
 import { PropsWithChildren } from "react";
+
 export default function Guest({ children }: PropsWithChildren) {
     return (
         <div className="flex flex-col sm:justify-center items-center bg-gray-100 pt-6 sm:pt-0 min-h-screen">
