c725139d3b8cbaa2698e94178a4e5a2d171d79b3
icebingsu <botchat549@gmail.com>
2024-10-20 05:23:44 +0700

    giao diện login

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -32,58 +32,68 @@ export default function Login({
       {status && (
         <div className="mb-4 font-medium text-green-600 text-sm">{status}</div>
       )}
-      <form onSubmit={submit}>
-        <div>
-          <InputLabel htmlFor="email" value="Email" />
-          <TextInput
+      <div
+        className="relative flex justify-end items-center h-screen"
+        style={{ marginLeft: "700px" }}
+      >
+        <div className="absolute h-[520px] w-[430px]">
+          <div className="absolute h-[200px] w-[200px] rounded-full bg-gradient-to-r from-[#1845ad] to-[#23a2f6] -top-[80px] -left-[80px]"></div>
+          <div className="absolute h-[200px] w-[200px] rounded-full bg-gradient-to-r from-[#ff512f] to-[#f09819] -bottom-[80px] -right-[30px]"></div>
+        </div>
+        <form
+          className="relative w-[400px] h-[520px] rounded-lg backdrop-blur-[10px] border-2 border-[rgba(255,255,255,0.1)] shadow-[0_0_40px_rgba(8,7,16,0.6)] p-[50px_35px]"
+          onSubmit={submit}
+        >
+          <h3 className="text-2xl font-medium text-white text-center flex justify-center items-center">
+            <img
+              src="https://fintown.software/imgs/logo.png"
+              alt=""
+              width={70}
+              height={70}
+              className="block"
+            />
+            <span> FinTown</span>
+          </h3>
+          <label
+            className="block mt-6 text-white text-lg font-medium"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
             id="email"
             type="email"
             name="email"
-            value={data.email}
-            className="block mt-1 w-full"
-            autoComplete="username"
-            isFocused={true}
             onChange={(e) => setData("email", e.target.value)}
+            placeholder="Email or Phone"
+            className="w-full h-[50px] bg-[rgba(255,255,255,0.07)] rounded-md p-2 mt-2 text-white text-sm"
           />
-          <InputError message={errors.email} className="mt-2" />
-        </div>
-        <div className="mt-4">
-          <InputLabel htmlFor="password" value="Password" />
-          <TextInput
+          <InputError message={errors.email} className="mt-2  font-bold" />
+          <label
+            className="block mt-6 text-white text-lg font-medium"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
             id="password"
             type="password"
             name="password"
+            placeholder="Password"
             value={data.password}
-            className="block mt-1 w-full"
-            autoComplete="current-password"
             onChange={(e) => setData("password", e.target.value)}
+            className="w-full h-[50px] bg-[rgba(255,255,255,0.07)] rounded-md p-2 mt-2 text-white text-sm"
           />
-          <InputError message={errors.password} className="mt-2" />
-        </div>
-        <div className="block mt-4">
-          <label className="flex items-center">
-            <Checkbox
-              name="remember"
-              checked={data.remember}
-              onChange={(e) => setData("remember", e.target.checked)}
-            />
-            <span className="text-gray-600 text-sm ms-2">Remember me</span>
-          </label>
-        </div>
-        <div className="flex justify-end items-center mt-4">
-          {canResetPassword && (
-            <Link
-              href={route("password.request")}
-              className="rounded-md focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 text-gray-600 text-sm hover:text-gray-900 underline focus:outline-none"
-            >
-              Forgot your password?
-            </Link>
-          )}
-          <PrimaryButton className="ms-4" disabled={processing}>
+          <InputError message={errors.password} className="mt-2 font-bold" />
+          <div className="mt-3">
+          <Checkbox name="remember"  checked={data.remember} onChange={(e) => setData("remember", e.target.checked)} />{" "}
+          <span className="text-white text-sm ms-2">Remember me</span>
+          </div>
+          <PrimaryButton className="mt-12 w-30 bg-[rgba(255,255,255,0.07)] py-3 text-lg font-semibold rounded-md cursor-pointer" disabled={processing} >
             Log in
           </PrimaryButton>
-        </div>
-      </form>
+        </form>
+      </div>
     </GuestLayout>
   );
 }
