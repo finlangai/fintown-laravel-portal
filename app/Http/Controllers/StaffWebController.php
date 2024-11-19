16fb6896d980ee17587651256ec057dc00c7de81
schmev <86550672+schmev91@users.noreply.github.com>
2024-11-19 10:26:29 +0700

    fix: only display staff roles on staff list

--- app/Http/Controllers/StaffWebController.php
+++ app/Http/Controllers/StaffWebController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\SQL\Staff\Staff; 
-use Illuminate\Support\Facades\Auth; 
+use App\Models\SQL\Staff\Staff;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
@@ -14,59 +14,64 @@ use Spatie\Permission\Traits\HasRoles;
 
 class StaffWebController extends Controller
 {
-    use Notifiable, HasRoles; 
+    use Notifiable, HasRoles;
 
     public function index()
     {
-        $staffList = Staff::with('roles.permissions')->get()->map(function($staff) {
-            return [
-                'id' => $staff->id,
-                'username' => $staff->username,
-                'fullname' => $staff->fullname,
-                'email' => $staff->email,
-                'roles' => $staff->roles->pluck('name'), 
-                'permissions' => $staff->roles->flatMap(function($role) {
-                    return $role->permissions->pluck('name'); 
-                })->unique(),
-                'role_type' => $staff->roles->pluck('name')->contains('super-admin') ? 'super-admin' : 'admin', 
-            ];
-        });
+        $staffList = Staff::with("roles.permissions")
+            ->get()
+            ->map(function ($staff) {
+                return [
+                    "id" => $staff->id,
+                    "username" => $staff->username,
+                    "fullname" => $staff->fullname,
+                    "email" => $staff->email,
+                    "roles" => $staff->roles->pluck("name"),
+                    "permissions" => $staff->roles
+                        ->flatMap(function ($role) {
+                            return $role->permissions->pluck("name");
+                        })
+                        ->unique(),
+                    "role_type" => $staff->roles
+                        ->pluck("name")
+                        ->contains("super-admin")
+                        ? "super-admin"
+                        : "admin",
+                ];
+            });
         $permissions = Permission::all();
-        $roles = Role::all();
+        $roles = Role::where("guard", "web")->get();
 
-        return Inertia::render('Staff/Staff', [
-            'role'=> $roles,
-            'staffList' => $staffList, 
-            'permissionsList' => $permissions,
+        return Inertia::render("Staff/Staff", [
+            "role" => $roles,
+            "staffList" => $staffList,
+            "permissionsList" => $permissions,
         ]);
     }
 
     public function updatePermissions(Request $request, $id)
     {
         $user = Staff::findOrFail($id);
-        $roles = $request->input('roles', []); 
-        DB::table('model_has_roles')->where('model_id', $user->id)->delete();
+        $roles = $request->input("roles", []);
+        DB::table("model_has_roles")
+            ->where("model_id", $user->id)
+            ->delete();
         if (!empty($roles)) {
-            $role = $roles[0]; 
-    
-            $roleRecord = DB::table('roles')->where('name', $role)->first();
-    
+            $role = $roles[0];
+
+            $roleRecord = DB::table("roles")->where("name", $role)->first();
+
             if ($roleRecord) {
-                DB::table('model_has_roles')->insert([
-                    'role_id' => $roleRecord->id,
-                    'model_type' => Staff::class, 
-                    'model_id' => $user->id,
+                DB::table("model_has_roles")->insert([
+                    "role_id" => $roleRecord->id,
+                    "model_type" => Staff::class,
+                    "model_id" => $user->id,
                 ]);
             }
         }
-    
-        return redirect()->back()->with('success', 'Cập nhật vai trò thành công!');
-    }
-    
-    
-    
 
-    
-    
-    
+        return redirect()
+            ->back()
+            ->with("success", "Cập nhật vai trò thành công!");
+    }
 }
